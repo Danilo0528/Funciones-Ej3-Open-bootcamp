f523f2341917bc568b07092c56ccdf930d72d374
Your Name~ <dpino131@gmail.com>
2023-01-23 12:51:28 -0500

    Practica de java ejercicio 3 open bootcamp

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/*public class Main {
+    /*Crear una función con tres parámetros que sean números que se suman entre sí.
+    Llamar a la función en el main y darle valores.
+    public static int suma (int a, int b, int c) {
+        return (a + b + c);
+    }
+    public static void main (String[]args){
+        int a = 10 , b = 10 , c = 10;
+        System.out.println(suma(a,b,c));
+    }
+}*/
